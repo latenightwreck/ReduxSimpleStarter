ef5565e7cb9d613d0e7fec4a7c2e8c24cb29b8c8
latenightwreck <morgan.b.armstrong@gmail.com>
2018-07-23 19:05:48 -0400

    actually remove boilerplate code. Test to create component to DOM

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-// create a new component. this component should produce some HTML
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+// create a new component. this component should produce some HTML
+const App = () => {
+  return <div>Hello!</div>;
+}
 
 //Take this coponent's generated HTML and put it on the page (in DOM).
+ReactDOM.render(<App />, document.querySelector('.container'));
