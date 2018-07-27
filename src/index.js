4b8489658b938f23fc09c5246d58f31612b6f283
latenightwreck <morgan.b.armstrong@gmail.com>
2018-07-27 15:45:01 -0400

    Added search functionality

--- src/index.js
+++ src/index.js
@@ -16,17 +16,22 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({key: YOUTUBE_API_KEY, term: 'achievement hunter'}, (videos) => {
+    this.videoSearch('achievement hunter');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: YOUTUBE_API_KEY, term: term}, (videos) => {
       this.setState({ 
         videos: videos,
         selectedVideo: videos[0]
       });
     });
   }
+
   render() {
      return (
     <div>
-      <SearchBar />
+      <SearchBar onSearchTermChange={ term => this.videoSearch(term) }/>
       <VideoDetail video={this.state.selectedVideo} />
       <VideoList 
         onVideoSelect={ selectedVideo => this.setState({ selectedVideo }) }
